6f82166d58792d1365602db3d7727b657b8b9cc4
Gleb Grig <89258282600g@gmail.com>
2020-05-11 22:26:26 +0300

    Добавьте файлы проекта.

---
+++ Grigorev_Gleb/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grigorev_Gleb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("НЕТ, НЕЛЬЗЯ КОМИК САНС ИСПОЛЬЗОВАТЬ АААААА.");
+            Close();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
