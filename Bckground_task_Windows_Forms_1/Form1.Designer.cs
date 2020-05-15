6b7aad4b06211d0642e106e4a443a4cc9d4577c8
Gleb Grig <89258282600g@gmail.com>
2020-05-15 21:34:02 +0300

    Фоновая Windows Forms 1

---
+++ Bckground_task_Windows_Forms_1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Bckground_task_Windows_Forms_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.A = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(50, 179);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(88, 75);
+            this.A.TabIndex = 0;
+            this.A.Text = "A";
+            this.A.UseVisualStyleBackColor = true;
+            this.A.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(464, 179);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(86, 75);
+            this.B.TabIndex = 1;
+            this.B.Text = "B";
+            this.B.UseVisualStyleBackColor = true;
+            this.B.Click += new System.EventHandler(this.B_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(578, 544);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Чудо-кнопки";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button B;
+    }
+}
+
---
+++ Bckground_task_Windows_Forms_1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bckground_task_Windows_Forms_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if ((A.Location.X + A.Width) <= (B.Location.X -20))
+            {
+                A.Location = new Point(A.Location.X + 10, A.Location.Y);
+                B.Location = new Point(B.Location.X- 10, B.Location.Y);
+            }
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            if (A.Location.X - 10 >= 0+ SystemInformation.BorderSize.Width)
+            {
+                A.Location = new Point(A.Location.X - 10, A.Location.Y);
+            }
+            int i = SystemInformation.BorderSize.Width;
+            if ((B.Location.X + B.Width+10) < Width- SystemInformation.BorderSize.Width) //-6 нужно, т.к граница в около 6 пикселей слева у окна не является частью окна
+            {
+                B.Location = new Point(B.Location.X + 10, B.Location.Y);
+            }
+        }
+    }
+}
